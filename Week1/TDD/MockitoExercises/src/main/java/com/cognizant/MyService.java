7ddb58b10aaa70341b521455977bb9723c9fdfe8
Hari-hara7 <Hariharanath247@gmail.com>
2026-06-19 15:45:18 +0530

    completed mockito exercise

---
+++ Week1/TDD/MockitoExercises/src/main/java/com/cognizant/MyService.java
@@ -0,0 +1,16 @@
+package com.cognizant;
+
+public class MyService {
+          private ExternalApi api;
+          
+          public MyService(ExternalApi api) {
+        	  this.api=api;
+          }
+          
+          public String fetchData() {
+        	  
+        	  return api.getData();
+          }
+          
+          
+}
